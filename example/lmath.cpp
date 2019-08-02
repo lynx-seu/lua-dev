9892bac551d2a902ca304ea3f0410c7a9b4b8360
hxl <wyy.hxl@gmail.com>
2019-08-02 23:39:54 +0800

    example

---
+++ example/lmath.cpp
@@ -0,0 +1,26 @@
+
+#include <math.h>
+extern "C" {
+#   include "lua.h"
+#   include "lualib.h"
+#   include "lauxlib.h"
+}
+
+static int lsin(lua_State *L) {
+    double n = luaL_checknumber(L, 1);
+    lua_pushnumber(L, sin(n));
+    return 1;
+}
+
+int luaopen_math2(lua_State *L) {
+    static const struct luaL_reg mathlib[] = {
+        {"sin", lsin},
+        {NULL, NULL},
+    }
+
+    lua_newtable(L);
+    luaL_setfuncs(L, mathlib, 0);
+    return 1;
+}
+
+
---
+++ example/test.lua
@@ -0,0 +1,6 @@
+
+local math = math
+local math2 = require "math2"
+
+assert(math.sin(30) == math2.sin(30))
+
